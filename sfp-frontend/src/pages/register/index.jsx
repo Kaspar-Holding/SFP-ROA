eb386a3fcc1907b6f9a573cb9e572077b8c81fd2
armughan03 <armughan.ahmad03@gmail.com>
2023-09-12 11:03:42 +0500

    11 September 2023 Update

--- sfp-frontend/src/pages/register/index.jsx
+++ sfp-frontend/src/pages/register/index.jsx
@@ -76,7 +76,7 @@ const RegisterPage = () => {
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">Email address</label>
                             <input name="email" value={email} onChange={(e)=>{onInputUpdate(e)}} type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                            <div id="emailHelp" class="form-text">We will never share your email with anyone else.</div>
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputPassword1" class="form-label">Password</label>
